7ae21f1601851bab572ec4947a3dd698fa49f3c9
Taka499 <14145862+Taka499@users.noreply.github.com>
2021-10-19 23:48:29 -0400

    added Search function

--- GUIProject/src/texteditor/FileUtilities.java
+++ GUIProject/src/texteditor/FileUtilities.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class FileUtilities {
 	public StringBuilder readFile(String filename)
 	{
+			// read the file and save the text in stringbuilder
 		File file = new File(filename);
 		StringBuilder fileContent = new StringBuilder();
 		try
@@ -32,7 +33,28 @@ public class FileUtilities {
 	
 	public int[] searchAll(StringBuilder fileContent, String searchString)
 	{
-		return null;
+			// will return the indexes of searched strings
+		int countWords = 0; // count the words before each substring
+		int numSearchString = 0;
+		int[] searchIndex = null;
+		String s = fileContent.toString(); 
+		StringBuilder indexList = new StringBuilder(); // record the index of each searched string
+		while (s.contains(searchString))
+		{
+			numSearchString++;
+			indexList.append(Integer.toString(s.indexOf(searchString)+countWords)+',');
+			countWords += s.substring(0, s.indexOf(searchString)+searchString.length()).length();
+			s = s.substring(s.indexOf(searchString)+searchString.length());
+		}
+		if (numSearchString > 0)
+		{
+			searchIndex = new int[numSearchString];
+			for (int i=0; i<numSearchString; i++)
+			{
+				searchIndex[i] = Integer.valueOf(indexList.toString().split(",")[i]);
+			}
+		}
+		return searchIndex;
 	}
 	
 	public int replace(StringBuilder fileContent, String oldString, String newString)
--- GUIProject/src/texteditor/TextEditor.java
+++ GUIProject/src/texteditor/TextEditor.java
@@ -5,21 +5,31 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.paint.Color;
 import javafx.scene.layout.CornerRadii;
+import javafx.scene.text.Text;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.SeparatorMenuItem;
+import javafx.stage.Window;
+import javafx.stage.Modality;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.application.Platform;
+import javafx.beans.binding.BooleanBinding;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 
 import java.io.File;
 import java.nio.file.Paths;
@@ -41,7 +51,7 @@ public class TextEditor extends Application {
 	{
 		this.stage = stage;
 		setScreen();
-		Scene scene = new Scene(root, 500, 600);
+		Scene scene = new Scene(root, 960, 540);
 		stage.setTitle("Text Reader"); //has only reading functions
 		stage.setScene(scene);
 		stage.show();
@@ -129,10 +139,128 @@ public class TextEditor extends Application {
 	// then other Handler...
 	private class SearchToolHandler implements EventHandler<ActionEvent>
 	{
+		TextField userTextField = new TextField();
+		Text numSearched = new Text();
+		String searchString;
+		int[] indexList;
+		int currentIndex;
+		
 		@Override
 		public void handle(ActionEvent event)
 		{
+				// create a new stage(window pop-up)
+			Stage searchStage = new Stage();
+			searchStage.initOwner(stage);
+				// you can continue to use the editor while a search window is opening.
+			searchStage.initModality(Modality.NONE);
 			
+				// set GridPane and add it to Scene
+			GridPane searchRoot = new GridPane();
+			searchRoot.setAlignment(Pos.CENTER);
+			searchRoot.setHgap(10);
+			searchRoot.setVgap(10);
+			Scene searchScene = new Scene(searchRoot, 350, 120);
+				// create child nodes
+			Button nextBtn = new Button("Search Next");
+			Button prevBtn = new Button("Previous");
+			Text searchText = new Text("Search for:");
+			
+				// disable the buttons when text field is empty
+			//BooleanBinding nextBtnBB = user
+			nextBtn.disableProperty().bind(new BooleanBinding()
+			{
+				{
+					super.bind(userTextField.textProperty());
+				}
+				
+				@Override
+				protected boolean computeValue()
+				{
+					return userTextField.getText().isEmpty();
+				}
+			});
+				// ToDo: disable the previous button when text field is empty or the next button wasn't used
+			/*prevBtn.disableProperty().bind(new BooleanBinding()
+			{
+				{
+					super.bind(userTextField.textProperty());
+				}
+				
+				@Override
+				protected boolean computeValue()
+				{
+					return currentIndex < 0 ? true:userTextField.getText().isEmpty();
+				}
+			});*/
+				// set EventHandler
+			nextBtn.setOnAction(new nextButtonHandler());
+			prevBtn.setOnAction(new previousButtonHandler());
+				// allow ENTER key to search
+			userTextField.setOnKeyPressed(new EventHandler<KeyEvent>()
+			{
+				@Override
+				public void handle(KeyEvent e)
+				{
+					if (e.getCode().equals(KeyCode.ENTER))
+					{
+						nextBtn.fire();
+					}
+				}
+			});
+			
+				// add nodes to GridPane
+			searchRoot.add(searchText, 0, 0);
+			searchRoot.add(userTextField, 1, 0, 2, 1);
+			searchRoot.add(prevBtn, 3, 1);
+			searchRoot.add(nextBtn, 3, 0);
+			searchRoot.add(numSearched, 2, 1);
+			searchRoot.setGridLinesVisible(true); // ToDo: set to fault after all the coding
+			
+				// button need to be able to select the searching string, and by clicking the next button
+				// move to next searched string. 
+			
+			searchStage.setTitle("Search...");
+			searchStage.setScene(searchScene);
+			searchStage.show();
+		}
+		
+		// handler to handle Next Search button
+		private class nextButtonHandler implements EventHandler<ActionEvent>
+		{
+			@Override
+			public void handle(ActionEvent event)
+			{
+				if (!userTextField.getText().equals(searchString) || searchString.isEmpty())
+				{
+					currentIndex = -1;
+					numSearched.setText("");
+					searchString = userTextField.getText();
+					indexList = fileUtilities.searchAll(fileContent, searchString);
+					numSearched.setVisible(true);
+				}
+				currentIndex = (currentIndex+1)%indexList.length;
+				fileTextArea.selectRange(indexList[currentIndex], 
+						indexList[currentIndex]+searchString.length());
+				userTextField.selectAll();
+				numSearched.setText(String.format("%d found, showing %d/%d", 
+						indexList.length, currentIndex+1, indexList.length));
+			}
+		}
+		
+		// handler to handle Previous button
+		private class previousButtonHandler implements EventHandler<ActionEvent>
+		{
+			@Override
+			public void handle(ActionEvent event)
+			{
+				currentIndex = currentIndex-1 < 0 ? 
+						indexList.length+currentIndex-1:(currentIndex-1)%indexList.length;
+				fileTextArea.selectRange(indexList[currentIndex], 
+						indexList[currentIndex]+searchString.length());
+				userTextField.selectAll();
+				numSearched.setText(String.format("%d found, showing %d/%d", 
+						indexList.length, currentIndex+1, indexList.length));
+			}
 		}
 	}
 	
