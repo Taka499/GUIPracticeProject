072488bb67fdbcf4986aa0d801d89f7a35f3c296
Taka499 <14145862+Taka499@users.noreply.github.com>
2021-11-29 12:34:52 -0500

    Added new features: save file;disable some buttons when while is not open

---
+++ FileUtilities.java
@@ -0,0 +1,81 @@
+package texteditor;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class FileUtilities {
+	public StringBuilder readFile(String filename)
+	{
+			// read the file and save the text in stringbuilder
+		File file = new File(filename);
+		StringBuilder fileContent = new StringBuilder();
+		try
+		{
+			Scanner input = new Scanner(file);
+			while(input.hasNextLine())
+			{
+				fileContent.append(input.nextLine() + "\n");
+			}
+			input.close();
+		}
+		catch (FileNotFoundException e)
+		{
+			System.out.println(e.getMessage());
+		}
+		return fileContent;
+	}
+	
+	public boolean writeFile(String filename, StringBuilder fileContent)
+	{
+			// write the file
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(filename)))
+		{
+			bw.write(fileContent.toString());
+		}
+		catch (IOException e)
+		{
+			return false;
+		}
+		return true;
+	}
+	
+	public int countWords(StringBuilder fileContent)
+	{
+		return fileContent.toString().split("[\\n\\s\\,\\.\\;\\:\\(\\)]+").length;
+	}
+	
+	public int[] searchAll(StringBuilder fileContent, String searchString)
+	{
+			// will return the indexes of searched strings
+		int countWords = 0; // count the words before each substring
+		int numSearchString = 0;
+		int[] searchIndex = null;
+		String s = fileContent.toString(); 
+		StringBuilder indexList = new StringBuilder(); // record the index of each searched string
+		while (s.contains(searchString))
+		{
+			numSearchString++;
+			indexList.append(Integer.toString(s.indexOf(searchString)+countWords)+',');
+			countWords += s.substring(0, s.indexOf(searchString)+searchString.length()).length();
+			s = s.substring(s.indexOf(searchString)+searchString.length());
+		}
+		if (numSearchString > 0)
+		{
+			searchIndex = new int[numSearchString];
+			for (int i=0; i<numSearchString; i++)
+			{
+				searchIndex[i] = Integer.valueOf(indexList.toString().split(",")[i]);
+			}
+		}
+		return searchIndex;
+	}
+	
+	public int replace(StringBuilder fileContent, String oldString, String newString)
+	{
+		return 0;
+	}
+}
---
+++ TextEditor.java
@@ -0,0 +1,350 @@
+package texteditor;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.paint.Color;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.text.Text;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.stage.Window;
+import javafx.stage.Modality;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.application.Platform;
+import javafx.beans.binding.BooleanBinding;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+public class TextEditor extends Application {
+	Stage stage;
+	BorderPane root = new BorderPane();
+	TextArea fileTextArea = new TextArea();
+	Label statusLabel = new Label();
+	StringBuilder fileContent;
+	FileUtilities fileUtilities = new FileUtilities();
+	BooleanProperty isFileOpen = new SimpleBooleanProperty(false);
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage stage)
+	{
+		this.stage = stage;
+		setScreen();
+		Scene scene = new Scene(root, 960, 540);
+		stage.setTitle("Text Reader"); //has only reading functions
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public void setScreen()
+	{
+			//create menus
+		Menu fileMenu = new Menu("File");
+		Menu toolsMenu = new Menu("Tools");
+		Menu helpMenu = new Menu("Help");
+		
+			// create menu items
+		MenuItem openFileMenuItem = new MenuItem("Open File");
+		MenuItem saveFileMenuItem = new MenuItem("Save File");
+		MenuItem exitFileMenuItem = new MenuItem("Exit");
+		MenuItem closeFileMenuItem = new MenuItem("Close File");
+		MenuItem searchToolsMenuItem = new MenuItem("Search");
+		MenuItem replaceToolsMenuItem = new MenuItem("Replace");
+		MenuItem wordCountToolsMenuItem = new MenuItem("Word Count");
+		MenuItem aboutHelpMenuItem = new MenuItem("About");
+		
+			// attach event handlers
+		openFileMenuItem.setOnAction(new OpenFileHandler());
+		saveFileMenuItem.setOnAction(new SaveFileHandler());
+		searchToolsMenuItem.setOnAction(new SearchToolHandler());
+		replaceToolsMenuItem.setOnAction(new ReplaceToolHandler());
+		wordCountToolsMenuItem.setOnAction(new WordCountToolHandler());
+		aboutHelpMenuItem.setOnAction(new AboutHandler());
+		
+		
+		closeFileMenuItem.setOnAction(new EventHandler<ActionEvent>()
+		{
+			@Override
+			public void handle(ActionEvent event)
+			{
+				fileTextArea.clear();
+				statusLabel.setText("");
+				isFileOpen.set(false);
+				root.setCenter(null);
+			}
+		});
+		
+		exitFileMenuItem.setOnAction(actionEvent->Platform.exit());// lambda expression
+		
+			// set status bar width and color
+		statusLabel.setPrefWidth(this.stage.getMaxWidth());
+		statusLabel.setBackground(new Background(new BackgroundFill(Color.LIGHTGRAY, CornerRadii.EMPTY, Insets.EMPTY)));
+		
+			// add menu items to menus and menus to menubar
+		MenuBar menuBar = new MenuBar();
+		fileMenu.getItems().addAll(openFileMenuItem, saveFileMenuItem, closeFileMenuItem, new SeparatorMenuItem(), exitFileMenuItem);
+		toolsMenu.getItems().addAll(searchToolsMenuItem, replaceToolsMenuItem, new SeparatorMenuItem(), wordCountToolsMenuItem);
+		helpMenu.getItems().addAll(aboutHelpMenuItem);
+		menuBar.getMenus().addAll(fileMenu, toolsMenu, helpMenu);
+		
+			// disable the buttons when no file is open
+		BooleanBinding fileIsOpenBinding = new BooleanBinding()
+		{
+			{
+				super.bind(isFileOpen);
+			}
+
+			@Override
+			protected boolean computeValue()
+			{
+				// TODO Auto-generated method stub
+				return !isFileOpen.get();
+			}
+		};
+		saveFileMenuItem.disableProperty().bind(fileIsOpenBinding);
+		closeFileMenuItem.disableProperty().bind(fileIsOpenBinding);
+		searchToolsMenuItem.disableProperty().bind(fileIsOpenBinding);
+		replaceToolsMenuItem.disableProperty().bind(fileIsOpenBinding);
+		wordCountToolsMenuItem.disableProperty().bind(fileIsOpenBinding);
+		
+			// set root children
+		root.setTop(menuBar);
+		root.setBottom(statusLabel);
+	}// end setScreen()
+	
+	private class OpenFileHandler implements EventHandler<ActionEvent>
+	{
+		@Override
+		public void handle (ActionEvent event)
+		{
+			FileChooser fileChooser = new FileChooser();
+			fileChooser.setTitle("Select file");
+			String initialPath = Paths.get(".\\data").toAbsolutePath().normalize().toString();
+			fileChooser.setInitialDirectory(new File(initialPath)); //relative path of data folder
+			// if no setInitialDirectory(), will set to root folder of OS
+			fileChooser.getExtensionFilters().addAll(
+					new ExtensionFilter("Text Files", "*.txt"),
+					new ExtensionFilter("All Files", "*.*"));
+			File file = null;
+			if ((file = fileChooser.showOpenDialog(stage)) != null)
+			{
+				fileContent = fileUtilities.readFile(file.getAbsolutePath());
+				fileTextArea.clear();
+				fileTextArea.appendText(fileContent.toString());
+				fileTextArea.setWrapText(true);
+				fileTextArea.positionCaret(0);
+				statusLabel.setText(file.getAbsolutePath());
+				isFileOpen.set(true);
+				root.setCenter(fileTextArea);
+			}// end if
+		}// end handle()
+	}// end OpenFileHandler
+	
+	private class SaveFileHandler implements EventHandler<ActionEvent>
+	{
+
+		@Override
+		public void handle(ActionEvent event)
+		{
+			FileChooser fileChooser = new FileChooser();
+			fileChooser.setTitle("Save file");
+			String initialPath = Paths.get(".\\data").toAbsolutePath().normalize().toString();
+			fileChooser.setInitialDirectory(new File(initialPath));
+			fileChooser.getExtensionFilters().addAll(
+					new ExtensionFilter("Text Files", "*.txt"),
+					new ExtensionFilter("All Files", "*.*"));
+			File file = null;
+			if ((file = fileChooser.showSaveDialog(stage)) != null)
+			{
+				if (fileUtilities.writeFile(file.getAbsolutePath(), new StringBuilder(fileTextArea.getText())))
+				{
+					statusLabel.setText(String.format("Saved as %s", file.getAbsoluteFile()));
+				}
+				else
+				{
+					statusLabel.setText(String.format("Failed to save as %s", file.getAbsoluteFile()));
+				}
+			}
+		}
+		
+	}
+	
+	// then other Handler...
+	private class SearchToolHandler implements EventHandler<ActionEvent>
+	{
+		TextField userTextField = new TextField();
+		Text numSearched = new Text();
+		String searchString;
+		int[] indexList;
+		int currentIndex;
+		
+		@Override
+		public void handle(ActionEvent event)
+		{
+				// create a new stage(window pop-up)
+			Stage searchStage = new Stage();
+			searchStage.initOwner(stage);
+				// you can continue to use the editor while a search window is opening.
+			searchStage.initModality(Modality.NONE);
+			
+				// set GridPane and add it to Scene
+			GridPane searchRoot = new GridPane();
+			searchRoot.setAlignment(Pos.CENTER);
+			searchRoot.setHgap(10);
+			searchRoot.setVgap(10);
+			Scene searchScene = new Scene(searchRoot, 350, 120);
+				// create child nodes
+			Button nextBtn = new Button("Search Next");
+			Button prevBtn = new Button("Previous");
+			Text searchText = new Text("Search for:");
+			
+				// disable the buttons when text field is empty
+			//BooleanBinding nextBtnBB = user
+			nextBtn.disableProperty().bind(new BooleanBinding()
+			{
+				{
+					super.bind(userTextField.textProperty());
+				}
+				
+				@Override
+				protected boolean computeValue()
+				{
+					return userTextField.getText().isEmpty();
+				}
+			});
+				// ToDo: disable the previous button when text field is empty or the next button wasn't used
+			/*prevBtn.disableProperty().bind(new BooleanBinding()
+			{
+				{
+					super.bind(userTextField.textProperty());
+				}
+				
+				@Override
+				protected boolean computeValue()
+				{
+					return currentIndex < 0 ? true:userTextField.getText().isEmpty();
+				}
+			});*/
+				// set EventHandler
+			nextBtn.setOnAction(new nextButtonHandler());
+			prevBtn.setOnAction(new previousButtonHandler());
+				// allow ENTER key to search
+			userTextField.setOnKeyPressed(new EventHandler<KeyEvent>()
+			{
+				@Override
+				public void handle(KeyEvent e)
+				{
+					if (e.getCode().equals(KeyCode.ENTER))
+					{
+						nextBtn.fire();
+					}
+				}
+			});
+			
+				// add nodes to GridPane
+			searchRoot.add(searchText, 0, 0);
+			searchRoot.add(userTextField, 1, 0, 2, 1);
+			searchRoot.add(prevBtn, 3, 1);
+			searchRoot.add(nextBtn, 3, 0);
+			searchRoot.add(numSearched, 2, 1);
+			searchRoot.setGridLinesVisible(true); // ToDo: set to fault after all the coding
+			
+				// button need to be able to select the searching string, and by clicking the next button
+				// move to next searched string. 
+			
+			searchStage.setTitle("Search...");
+			searchStage.setScene(searchScene);
+			searchStage.show();
+		}
+		
+		// handler to handle Next Search button
+		private class nextButtonHandler implements EventHandler<ActionEvent>
+		{
+			@Override
+			public void handle(ActionEvent event)
+			{
+				if (!userTextField.getText().equals(searchString) || searchString.isEmpty())
+				{
+					currentIndex = -1;
+					numSearched.setText("");
+					searchString = userTextField.getText();
+					indexList = fileUtilities.searchAll(fileContent, searchString);
+					numSearched.setVisible(true);
+				}
+				currentIndex = (currentIndex+1)%indexList.length;
+				fileTextArea.selectRange(indexList[currentIndex], 
+						indexList[currentIndex]+searchString.length());
+				userTextField.selectAll();
+				numSearched.setText(String.format("%d found, showing %d/%d", 
+						indexList.length, currentIndex+1, indexList.length));
+			}
+		}
+		
+		// handler to handle Previous button
+		private class previousButtonHandler implements EventHandler<ActionEvent>
+		{
+			@Override
+			public void handle(ActionEvent event)
+			{
+				currentIndex = currentIndex-1 < 0 ? 
+						indexList.length+currentIndex-1:(currentIndex-1)%indexList.length;
+				fileTextArea.selectRange(indexList[currentIndex], 
+						indexList[currentIndex]+searchString.length());
+				userTextField.selectAll();
+				numSearched.setText(String.format("%d found, showing %d/%d", 
+						indexList.length, currentIndex+1, indexList.length));
+			}
+		}
+	}
+	
+	private class ReplaceToolHandler implements EventHandler<ActionEvent>
+	{
+		@Override
+		public void handle(ActionEvent event)
+		{
+			//TODO
+		}
+	}
+	
+	private class WordCountToolHandler implements EventHandler<ActionEvent>
+	{
+		@Override
+		public void handle(ActionEvent event)
+		{
+			//TODO
+		}
+	}
+	
+	private class AboutHandler implements EventHandler<ActionEvent>
+	{
+		@Override
+		public void handle(ActionEvent event)
+		{
+			//TODO
+		}
+	}
+}// end TextReader
